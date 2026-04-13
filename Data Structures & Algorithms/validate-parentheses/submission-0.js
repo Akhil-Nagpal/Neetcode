ec4db8d16162985ee7b69b97f11e977a869033a7
! Ak ! <96314234+Akhil-Nagpal@users.noreply.github.com>
2026-04-14 00:43:58 +0530

    Add: validate-parentheses - submission-0

---
+++ Data Structures & Algorithms/validate-parentheses/submission-0.js
@@ -0,0 +1,15 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isValid(s) {
+
+        for (let i = 0; i < s.length; i++) {
+            if (s.includes("()") || s.includes("{}") || s.includes("[]")) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
