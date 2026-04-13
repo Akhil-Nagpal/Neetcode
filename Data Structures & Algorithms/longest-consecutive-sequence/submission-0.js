24f9e317083200281f1a37b589150281ea534246
! Ak ! <96314234+Akhil-Nagpal@users.noreply.github.com>
2026-04-13 18:49:43 +0530

    Add: longest-consecutive-sequence - submission-0

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-0.js
@@ -0,0 +1,30 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    longestConsecutive(nums) {
+        
+        // Brute Force
+        let longest = 0;
+        let count = 1;
+
+        let sortedNums = nums.sort((a, b) => a - b); // sorted in acesding order
+
+
+        for (let i = 1; i < sortedNums.length; i++) {
+            if (sortedNums[i] === sortedNums[i - 1]) continue;
+            
+            if (sortedNums[i] === sortedNums[i - 1] + 1) {
+                count++;
+            } else {
+                // This step update when consecutive chain breaks
+                longest = Math.max(longest, count);
+                count = 1;
+            }
+        }
+        // This step update the longest when the array hit it's length
+        longest = Math.max(longest, count);
+        return longest;
+    }
+}
