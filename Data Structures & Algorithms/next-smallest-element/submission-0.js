2ec2f5b438e27e95fa57041237122517121d08e7
Akhil-Nagpal <akhilnagpal07@gmail.com>
2026-04-16 20:42:55 +0530

    Add: next-smaller-element - submission-0

--- Data Structures & Algorithms/next-smallest-element/submission-0.js
+++ Data Structures & Algorithms/next-smallest-element/submission-0.js
@@ -4,17 +4,15 @@ class Solution {
    * @returns {number[]}
    */
 
-  nextLargerElement(arr) {
-    // Code Here
+  nextSmallerElement(arr) {
     // Brute Force
 
     let result = [];
 
-    for (let i = i + 1; i < arr.length; i++) {
+    for (let i = 0; i < arr.length; i++) {
       let found = -1;
-
-      for (let j = 0; j < arr.length; j++) {
-        if (arr[j] > arr[i]) {
+      for (let j = i + 1; j < arr.length; j++) {
+        if (arr[j] < arr[i]) {
           found = arr[j];
           break;
         }
