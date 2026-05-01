46bf7bd78c3ce80bec4453b2b10edddf2b14bfb5
! Ak ! <96314234+Akhil-Nagpal@users.noreply.github.com>
2026-05-01 16:51:37 +0530

    Add: trapping-rain-water - submission-0

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-0.js
@@ -0,0 +1,30 @@
+class Solution {
+    /**
+     * @param {number[]} height
+     * @return {number}
+     */
+    trap(height) {
+        // Brute Force
+
+        let maxWater = 0;
+        let leftMax = 0;
+        let rightMax = 0;
+
+        for (let i = 0; i < height.length; i++) {
+            // find Left max
+            for (let j = 0; j < i; j++) {
+                leftMax = Math.max(leftMax, height[j]);
+            }
+            // find right max
+            for(let j = i; j < height.length; j++) {
+                rightMax = Math.max(rightMax, height[j]);
+            }
+
+            let water = Math.min(leftMax, rightMax) - height[i];
+
+            maxWater += water;
+        }
+        return maxWater;
+
+    }
+}
