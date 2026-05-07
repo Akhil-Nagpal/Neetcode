0d3ed5b795412b98a84fadfe7bf9ac2eb6a3500b
! Ak ! <96314234+Akhil-Nagpal@users.noreply.github.com>
2026-05-07 19:32:34 +0530

    Add: permutation-string - submission-1

---
+++ Data Structures & Algorithms/permutation-string/submission-1.js
@@ -0,0 +1,36 @@
+class Solution {
+    /**
+     * @param {string} s1
+     * @param {string} s2
+     * @return {boolean}
+     */
+    checkInclusion(s1, s2) {
+        // Brute Force
+
+        let targetMap = new Map();
+
+        for (let i = 0; i < s1.length; i++) {
+            targetMap.set(s1[i], (targetMap.get(s1[i]) || 0) + 1);
+        }
+
+        for (let i = 0; i < s2.length - s1.length; i++) {
+            let currentMap = new Map();
+            for (let j = i; j <= i + s1.length - 1; j++) {
+                currentMap.set(s2[j], (currentMap.get(s2[j]) || 0) + 1);
+            }
+
+            if (currentMap.size !== targetMap.size) continue;
+
+            let match = true;
+                
+            for (let [key, value] of targetMap) {
+                if (currentMap.get(key) !== value) {
+                    match = false;
+                    break;
+                }
+            }
+                if (match) return true;
+        }
+        return false;
+    }
+}
