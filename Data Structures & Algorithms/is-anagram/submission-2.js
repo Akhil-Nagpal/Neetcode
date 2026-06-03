1da21fe78606fc4ed3fe6dea45d785120e6de229
! Ak ! <96314234+Akhil-Nagpal@users.noreply.github.com>
2026-06-03 17:18:32 +0530

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.js
@@ -0,0 +1,16 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s, t) {
+        // Revision 
+
+        let sortS = s.split("").sort().join()
+        let sortT = t.split("").sort().join();
+
+        if (sortS === sortT) return true;
+        return false;
+    }
+}
