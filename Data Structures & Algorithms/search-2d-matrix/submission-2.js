48ee543bca618dcb02178a612229ccc935dd2586
! Ak ! <96314234+Akhil-Nagpal@users.noreply.github.com>
2026-06-06 23:42:19 +0530

    Add: search-2d-matrix - submission-2

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-2.js
@@ -0,0 +1,14 @@
+class Solution {
+    /**
+     * @param {number[][]} matrix
+     * @param {number} target
+     * @return {boolean}
+     */
+    searchMatrix(matrix, target) {
+        // Optimised code using Bineary Search
+
+        let left = 0;
+        let right = matrix.length - 1;
+        
+    }
+}
