59c498e938bdeb0d0163805fc812b800bc190e16
! Ak ! <96314234+Akhil-Nagpal@users.noreply.github.com>
2026-06-06 16:44:56 +0530

    Add: binary-search - submission-1

---
+++ Data Structures & Algorithms/binary-search/submission-1.js
@@ -0,0 +1,18 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number}
+     */
+    search(nums, target) {
+        // Optimised Code using Binary Search
+
+        // start from both ends left and right
+        let left = 0;
+        let right = nums.length - 1;
+        
+        while(left <= right) {
+            
+        }
+    }
+}
