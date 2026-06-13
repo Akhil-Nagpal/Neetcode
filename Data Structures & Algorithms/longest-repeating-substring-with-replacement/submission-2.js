2a5bdd0c597306d2975c7f1180ff63b364240d8a
! Ak ! <96314234+Akhil-Nagpal@users.noreply.github.com>
2026-06-13 18:29:24 +0530

    Add: longest-repeating-substring-with-replacement - submission-2

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-2.js
@@ -0,0 +1,29 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {number} k
+     * @return {number}
+     */
+    characterReplacement(s, k) {
+        // Revision 
+        let max = 0;
+
+        for (let i = 0; i < s.length; i++) {
+            let map = new Map();
+            let mapFreq = 0;
+            for (let j = i; j < s.length; j++) {
+                map.set(s[j], (map.get(s[j]) || 0) + 1 );
+                mapFreq = Math.max(mapFreq, map.get(s[j]));
+
+                let changesNeeded = (j - i + 1) - mapFreq;
+
+                if (changesNeeded <= k) {
+                    max = Math.max(j - i + 1, max)
+                } else {
+                    break;
+                }
+            }
+        }
+        return max;
+    }
+}
