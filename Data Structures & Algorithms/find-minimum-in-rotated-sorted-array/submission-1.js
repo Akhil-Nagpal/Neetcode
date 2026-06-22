97172a0e63d2761f8d660025012356210ff5b3c5
! Ak ! <96314234+Akhil-Nagpal@users.noreply.github.com>
2026-06-22 16:54:04 +0530

    Add: find-minimum-in-rotated-sorted-array - submission-1

---
+++ Data Structures & Algorithms/find-minimum-in-rotated-sorted-array/submission-1.js
@@ -0,0 +1,10 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    findMin(nums) {
+        // Brute Force without any variable
+        return Math.min(...nums);
+    }
+}
