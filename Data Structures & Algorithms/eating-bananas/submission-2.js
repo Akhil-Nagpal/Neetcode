205eccab0aec4b9b609e35124394603fa3e034ed
! Ak ! <96314234+Akhil-Nagpal@users.noreply.github.com>
2026-06-22 15:38:35 +0530

    Add: eating-bananas - submission-2

---
+++ Data Structures & Algorithms/eating-bananas/submission-2.js
@@ -0,0 +1,12 @@
+class Solution {
+    /**
+     * @param {number[]} piles
+     * @param {number} h
+     * @return {number}
+     */
+    minEatingSpeed(piles, h) {
+        // Optimised Code using binary search
+        // Remember we need to calculate speed so every thing will be based on calculating speed
+        let left = 1; // minimum possible eating speed
+    }
+}
